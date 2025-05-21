82d9570c4ccdb9e34568daec646d6a99faf6a3f9
KarimPortfolio <mohamedkarmballa19@gmail.com>
2025-05-21 11:59:50 +0100

    feature:  Ingrated sanctum api auth along with fortify for a stateful auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api([
+            EnsureFrontendRequestsAreStateful::class,
+            ThrottleRequests::class.':api',
+            SubstituteBindings::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\Api\V1\SshKeyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')
+Route::middleware('auth:sanctum')
+->prefix('v1')
 ->name('api.v1.')
 ->group(function () {
     Route::apiResource('ssh-keys', SshKeyController::class);
