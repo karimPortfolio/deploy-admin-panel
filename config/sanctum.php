0cb8685866995428568e741541ebc8b75680e7fc
KarimPortfolio <mohamedkarmballa19@gmail.com>
2025-11-10 00:07:02 +0100

    Update: added prod domain to sanctum config

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,deploy-admin-panel.test',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,deploy-admin-panel.test,ec2-34-229-200-45.compute-1.amazonaws.com',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
